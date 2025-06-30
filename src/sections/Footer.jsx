4149cada498813e4f4ed2acb76599232a6d2e821
ROHIT DUBEY <rohitdubey713010@gmail.com>
2025-06-30 09:44:16 +0530

    Worked on responsiveness of Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,17 +7,17 @@ const Footer = () => {
   return (
     <div className='bg-white py-10   text-black text-center '>
         <p className='font-medium '>[find us online]</p>
-      <p className='anton-font text-8xl '>@DANDOO</p>
+      <p className='anton-font text-6xl  md:text-7xl  lg:text-8xl '>@DANDOO</p>
       <div className='flex '>
 
       <div className='flex  w-full    py-2  justify-evenly  '>
-        <div className='flex flex-row py-2 gap-2 scale-150  '>
+        <div className='flex flex-row py-2 gap-2 scale-100  md:scale-150  '>
          <FaLinkedin className='cursor-pointer' />
          <FaInstagram className='cursor-pointer' />
          <FaSquareXTwitter className='cursor-pointer' />
         </div>
         <div className='flex flex-row '>
-            <p className='text-xl font-bold'> Terms | Policy | 2025 </p>
+            <p className='text-base md:text-xl font-bold'> Terms | Policy | 2025 </p>
         </div>
       </div>
       </div>
